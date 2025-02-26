83a44c1a7e0b9259e3dc0dfd6a28b35344dba663
ajensen25 <aidenjensen707@gmail.com>
2025-02-26 18:08:15 -0500

    added word selector and hint generation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import wordList from './data/wordList.js'
+
 // Generating keyboard buttons
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -7,4 +9,30 @@ alphabet.forEach((letter) => {
   const button = document.createElement('button');
   button.innerHTML = letter;
   keyboard.appendChild(button);
+  button.addEventListener('click', () => {
+    buttonClick(button);
+  });
 });
+
+let currentWord = {};
+
+function selectWord() {
+  const listIndex = Math.floor(Math.random() * wordList.length);
+  currentWord = wordList[listIndex];
+}
+
+const hint = document.querySelector('.js-hint');
+
+function startGame() {
+  selectWord();
+  hint.innerHTML = `
+    Hint: <b>${currentWord.hint}</b>
+  `;
+  console.log(currentWord.word)
+}
+
+function buttonClick(button) {
+  console.log(button.innerHTML);
+}
+
+startGame();
