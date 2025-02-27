537eeb033d271cab31f93157db88f59ede08a05c
ajensen25 <aidenjensen707@gmail.com>
2025-02-27 13:40:47 -0500

    added replay functionality

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ import wordList from './data/wordList.js'
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const keyboard = document.querySelector('.js-keyboard');
+const hangmanImage = document.querySelector('.js-hangman');
+const gameOverDisplay = document.querySelector('.js-game-over');
 
 alphabet.forEach((letter) => {
   const button = document.createElement('button');
@@ -14,7 +16,19 @@ alphabet.forEach((letter) => {
   });
 });
 
-let currentWord = {};
+let currentWord;
+let incorrectGuesses = 0;
+const maxGuesses = 6;
+
+const guesses = document.querySelector('.js-guesses');
+
+function renderGuesses() {
+  guesses.innerHTML = 
+  `
+    Incorrect guesses: <b>${incorrectGuesses} / ${maxGuesses}</b>
+  `;
+}
+
 
 function selectWord() {
   const listIndex = Math.floor(Math.random() * wordList.length);
@@ -28,11 +42,60 @@ function startGame() {
   hint.innerHTML = `
     Hint: <b>${currentWord.hint}</b>
   `;
-  console.log(currentWord.word)
+  hangmanImage.src = 'images/hangman-0.svg';
+  gameOverDisplay.classList.add('hidden');
+  console.log(currentWord.word);
 }
 
 function buttonClick(button) {
-  console.log(button.innerHTML);
+  const letter = button.innerHTML;
+  if (currentWord.word.includes(letter)) {
+    console.log(letter, 'is in ', currentWord.word);
+    gameOver('won');
+  }
+  else {
+    incorrectGuesses += 1;
+    hangmanImage.src = `images/hangman-${incorrectGuesses}.svg`;
+    renderGuesses();
+    if (incorrectGuesses >= maxGuesses) {
+      gameOver('lost');
+    }
+  }
+}
+
+function gameOver(status) {
+  if (status === 'lost') {
+    gameOverDisplay.innerHTML = 
+    `
+      <div class="game-over-container">
+        <img src="images/lost.gif" alt="You Lost!">
+        <h2>Game Over!</h2>
+        <p>The correct word was: &nbsp;<b>${currentWord.word}</b></p>
+        <button class="js-play-again">PLAY AGAIN</button>
+      </div>
+    `;
+    gameOverDisplay.classList.remove('hidden');
+    addButtonEvent();
+  } else if (status === 'won') {
+    gameOverDisplay.innerHTML = 
+    `
+      <div class="game-over-container">
+        <img src="images/victory.gif" alt="You Won!">
+        <h2>Game Over!</h2>
+        <p>You guessed the word&nbsp;<b>${currentWord.word}</b>&nbsp;correctly!</p>
+        <button class="js-play-again">PLAY AGAIN</button>
+      </div>
+    `;
+    gameOverDisplay.classList.remove('hidden');
+    addButtonEvent();
+  }
+}
+
+function addButtonEvent() {
+  const playAgainButton = document.querySelector('.js-play-again');
+  playAgainButton.addEventListener('click', () => {
+    startGame();
+  });
 }
 
 startGame();
